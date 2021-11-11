cbee39a081c41a05792435f32f2680671df94526
T3NZ0R <t3nz000r@gmail.com>
2021-11-11 19:04:37 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,311 @@
+//_____________HomeWork____________
+//_____________Перше_Завдання____________
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+//
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 =  123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+//
+// let bool1 = true;
+// let bool2 = false;
+//
+// console.log(a);
+// document.write(a + ' ');
+//  alert(a);
+//
+// console.log(b);
+// document.write(b + ' ');
+//  alert(b);
+//
+// console.log(c);
+// document.write(c + ' ');
+// alert(c);
+//
+// console.log(d);
+// document.write(d + ' ');
+// alert(d);
+//
+// console.log(num1);
+// document.write(num1 + ' ');
+// alert(num1);
+//
+// console.log(num2);
+// document.write(num2 + ' ');
+// alert(num2);
+//
+// console.log(num3);
+// document.write(num3 + ' ');
+// alert(num3);
+//
+// console.log(num4);
+// document.write(num4 + ' ');
+// alert(num4);
+//
+// console.log(num5);
+// document.write(num5 + ' ');
+// alert(num5);
+//
+// console.log(num6);
+// document.write(num6 + ' ');
+// alert(num6);
+//
+// console.log(num7);
+// document.write(num7 + ' ');
+// alert(num7);
+//
+// console.log(bool1);
+// document.write(bool1 + ' ');
+// alert(bool1);
+//
+// console.log(bool2);
+// document.write(bool2 + ' ');
+// alert(bool2);
+
+//_____________Друге_Завдання____________
+
+//  a = 'hello new';
+//  b = 'owu new';
+//  c = 'com new';
+//  d = 'ua new';
+//
+//  num1 = 2;
+//  num2 = 11;
+//  num3 = -998;
+//  num4 =  124;
+//  num5 = 3.15;
+//  num6 = 2.8;
+//  num7 = 17;
+//
+//  bool1 = false;
+//  bool2 = true;
+//
+// console.log(a);
+// document.write(a + ' ');
+// alert(a);
+//
+// console.log(b);
+// document.write(b + ' ');
+// alert(b);
+//
+// console.log(c);
+// document.write(c + ' ');
+// alert(c);
+//
+// console.log(d);
+// document.write(d + ' ');
+// alert(d);
+//
+// console.log(num1);
+// document.write(num1 + ' ');
+// alert(num1);
+//
+// console.log(num2);
+// document.write(num2 + ' ');
+// alert(num2);
+//
+// console.log(num3);
+// document.write(num3 + ' ');
+// alert(num3);
+//
+// console.log(num4);
+// document.write(num4 + ' ');
+// alert(num4);
+//
+// console.log(num5);
+// document.write(num5 + ' ');
+// alert(num5);
+//
+// console.log(num6);
+// document.write(num6 + ' ');
+// alert(num6);
+//
+// console.log(num7);
+// document.write(num7 + ' ');
+// alert(num7);
+//
+// console.log(bool1);
+// document.write(bool1 + ' ');
+// alert(bool1);
+//
+// console.log(bool2);
+// document.write(bool2 + ' ');
+// alert(bool2);
+
+//_____________Третє_Завдання____________
+
+// let firstName = 'Штарк';
+// let middleName = 'Віталій';
+// let lastName = 'Андрійович';
+//
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+//
+// console.log(person);
+
+//_____________Четверте_Завдання____________
+
+// let name = prompt("Введіть ваше ім'я");
+// let fName = prompt("Введіть ваше по-батькові");
+// let age = prompt("Введіть ваш вік");
+//
+// console.log(`Вітаю ${name} ${fName}. Тобі ${age} років.`);
+
+//_____________П'яте_Завдання____________
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//_____________Шосте_Завдання____________
+
+// console.log((5 !== 6));
+// console.log((5 > 6));
+// console.log((5 === 6));
+// console.log((5 >= 6));
+//
+// console.log((10 === 10));
+// console.log((10 >= 10));
+// console.log((10 > 10));
+// console.log((10 !== 10));
+// console.log((10 < 10));
+//
+// console.log((123 === '123'));
+// console.log((123 == '123'));
+
+//_____________Сьоме_Завдання____________
+
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); //205 - string + int
+// document.write(str - a + "<br/>"); //15  - стрінг стає інтом
+// document.write(str * "2" + "<br/>"); //40  - множення
+// document.write(str / 2 + "<br/>"); //10 - ділення
+
+//_____________ClassWork____________
+
+//_____________Перше_Завдання____________
+
+// let arr = [];
+// for(let i = 0; i<10; i++){
+//     arr[i]=Math.round(Math.random() * 100);
+// }
+// console.log(arr);
+//
+// let result = 0;
+//
+// for(let i = 0; i<10; i++) {
+// result+=arr[i];
+// }
+//
+// console.log(`Сума = ${result}`);
+
+//_____________Друге_Завдання____________
+
+// let book = {
+//     name: 'Місто',
+//     pages: 300,
+//     genre: "урбаністичний роман"
+// }
+//
+// console.log(book);
+
+//_____________Третє_Завдання____________
+
+// let book = {
+//     name: 'Місто',
+//     pages: 300,
+//     genre: "урбаністичний роман",
+//     author: 'Валер`ян Підмогильний'
+// }
+//
+// console.log(book);
+
+//_____________Четверте_Завдання____________
+
+// let arr = [
+//     book,
+//     {
+//         name: 'Місто-1',
+//         pages: 310,
+//         genre: "урбаністичний роман-1",
+//         author: 'Валер`ян Підмогильний'
+//     },
+//
+//     {
+//         name: 'Місто-2',
+//         pages: 320,
+//         genre: "урбаністичний роман-2",
+//         author: 'Валер`ян Підмогильний'
+//     },
+//
+//     {
+//         name: 'Місто-3',
+//         pages: 330,
+//         genre: "урбаністичний роман-3",
+//         author: 'Валер`ян Підмогильний'
+//     }
+// ]
+//
+//     for(let i = 0; i<4; i++)
+//     {
+//         console.log(arr[i]);
+//     }
+
+//_____________П'яте_Завдання____________
+
+// let height = 23;
+// let width = 10;
+//
+// let s = height * width;
+//
+// console.log(`Площа прямокутника = ${s} см^2`);
+
+//_____________Шосте_Завдання____________
+
+// const PI = 3.14;
+// let heightC = 10;
+// let dC = 4;
+// let r = dC/2;
+// let vHelp = (PI * Math.pow(r,2)) * heightC;
+// let v = vHelp.toFixed(1);
+//
+// console.log(`Об'єм циліндра = ${v} м^3`);
+
+//_____________Сьоме_Завдання____________
+
+// let n = 3;
+// let m = 4;
+// let c = Math.pow(m, 2) + Math.pow(n, 2);
+// let k = Math.sqrt(c);
+// console.log(`Довжина гіпотенузи = ${k} см.`)
+
+//_____________Errors____________
+
+// let a = 100;
+// let b = 500;
+// let c = "hello";
+// let d = "okten";
+// let x = 'constant value';
+// {
+//     x = 'new value';
+//     console.log(a);
+// }
+//
+// b = 'new value';
+//
+// let result = 100 + a;
+//
+// console.log(result);
+
+
